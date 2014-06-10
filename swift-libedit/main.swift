20efc2b08f32a30227af9df5d7d265ba43b98aad
Neil Pankey <neil@wire.im>
2014-06-10 01:28:44 -0700

    Initial commit

---
+++ swift-libedit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-libedit
+//
+//  Created by Neil Pankey on 6/10/14.
+//  Copyright (c) 2014 Neil Pankey. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
